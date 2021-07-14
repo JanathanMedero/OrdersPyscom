7f13387ceff31446d2300d50c1eb4d2c2ea20808
JanathanMedero <medero.janathan@gmail.com>
2021-07-14 10:27:07 -0500

    Design Table clients complete

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -1,10 +1,54 @@
 @extends('layouts.admin')
 
 @section('content')
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<div class="col-md-12">
+	<div class="card card-stats mx-4 my-4">
+		<div class="row">
+			<div class="col-md-12 d-flex justify-content-center my-4">
+				<h3 class="display-4">Tabla de Clientes</h3>
+			</div>
+		</div>
+		<div class="card-body">
+
+			<div class="table-responsive">
+				<div>
+					<table class="table align-items-center">
+						<thead class="thead-light">
+							<tr>
+								<th scope="col" class="sort" data-sort="name">Nombre</th>
+								<th scope="col" class="sort" data-sort="budget">Teléfono</th>
+								<th scope="col" class="sort" data-sort="status">Servicios</th>
+								
+								<th scope="col" class="sort" data-sort="completion">Acciones</th>
+								<th scope="col"></th>
+							</tr>
+						</thead>
+						<tbody class="list">
+							<tr>
+								<th scope="row">
+									<div class="media align-items-center">
+										<div class="media-body">
+											<span class="name mb-0 text-sm">Argon Design System</span>
+										</div>
+									</div>
+								</th>
+								<td class="budget">
+									4432256669
+								</td>
+								<td class="budget">
+									4 servicios
+								</td>
+								<td>
+									<button type="button" class="btn btn-info">Editar</button>
+									<button type="button" class="btn btn-danger">Eliminar</button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+		</div>    
+	</div>
+</div>
 @endsection
